17b059a9c2080982bf60ef79e13388cce5f672c3
Christian Medeiros Adriano <christian.adriano@gmail.com>
2013-05-28 01:01:35 -0700

    Manage Teacher Profiles and Notify Absence.

--- html/js/profileOperations.js
+++ html/js/profileOperations.js
@@ -1,5 +1,64 @@
 
+
+// Data to be used
+
+//Subjects and Schools without teacher
+var missingList = new Array(); 
+missingList[0] = ["Science","Pine"];
+missingList[1] = ["Geography","Oak"];
+
+//List of all substitute teachers, it is an Array of Arrays
+var hiredTeacherList = new Array();
+hiredTeacherList[0] = [["Mary Anning"], ["Science"], ["K","J"], ["Mahogany","Pine"]];
+hiredTeacherList[1] = [["Mary Sommervile"], ["History","Geography"], ["H","A"], ["Oak","Adobe"]];
+hiredTeacherList[2] = [["Lise Meitner"], ["Math","Science"], ["H"], ["Sequoia","Cedar"]];
+
+
+
+var subTeacherList = new Array();
+subTeacherList[0] = [["Marcus Antonius"], ["Science"], ["K","J"], ["Mahogany","Pine"]];
+subTeacherList[1] = [["Joana D'Arc"], ["History","Geography"], ["H","A"], ["Oak","Adobe"]];
+subTeacherList[2] = [["Marie Curie"], ["Math","Science"], ["H"], ["Sequoia","Cedar"]];
+subTeacherList[3] = [["Caroline Herschel"], ["Science"], ["A"], ["Pine","Cedar"]];
+
+
+
+
+
 //File handles the events sent by html pages from Sububi
+
+
+
+
+//-------------------------- Search function --------------------------------------------------
+function selectTeachersForSubjectSchoolList(subject, schoolList){
+	console.log("Selecting subs for subject "+ subject+ "in schools: "+schoolList);
+	return new Array();
+}
+
+function searchTeacherByName() {
+    var input = document.getElementById('teacherName');
+    var name = input.value;
+    var registry=new Array();
+    if (name) {
+       registry = searchName(name);
+       display(registry);
+    } else {
+        alert('Please enter a teacher name');
+        input.focus();
+    }
+}
+
+
+function searchName(name){
+	var registryResult = new Array();
+	
+	return registry;
+}
+
+function display(registry){}
+
+
 
 
 //------------------------- TEACHER ABSENCE PAGE -----------------------------------------------
@@ -18,11 +77,12 @@
 	function saveSubstitutePreferences (name, lastname, schoolList, subjectlist){
 		console.log("Saving Substitute Preferences, name: "+name+", last name:"+lastName);
 		console.log("School list:");
-		int i=0;
+		var i=0;
 		var schoolListStr;
 		if(schoolList.length()>0){
 			while(i<schoolList.lenght()){
 				schoolListStr = schoolListStr+schoolList[i]+";";
+				i++;
 			}
 			console.log(schoolListStr);
 		}
